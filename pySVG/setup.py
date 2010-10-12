67a60a4d5a92e59c5378865e6a1dcdde6bd5883e
kerim.mansour <kerim.mansour@7e4b4840-ada2-11de-9d95-573a147a3985>
2010-10-12 08:54:59 +0000

    -created new dist -changed license
    git-svn-id: http://pysvg.googlecode.com/svn/trunk@27 7e4b4840-ada2-11de-9d95-573a147a3985

--- pySVG/setup.py
+++ pySVG/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name="pysvg",
-      version="0.2.0",
+      version="0.2.1",
       description="Python SVG Library",
       author="Kerim Mansour",
       author_email="kmansour@web.de",
@@ -13,7 +13,7 @@ setup(name="pysvg",
       classifiers = [
           'Development Status :: 4 - Beta',
           'Intended Audience :: Developers',
-          'License :: Free for non-commercial use',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
           'Operating System :: MacOS :: MacOS X',
           'Operating System :: Microsoft :: Windows',
           'Operating System :: POSIX',
